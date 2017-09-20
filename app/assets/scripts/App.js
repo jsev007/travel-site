c9d423e3a3889015aa8345d57a2616d2dc4f9b50
Jorge Sevilla <jorgesevilla7@gmail.com>
2017-09-19 23:10:57 -0700

    completed desktop and mobile header styles

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,26 @@
+import MobileMenu from './modules/MobileMenu';
+
+var mobileMenu = new MobileMenu();
+
+/* // Example ...
+
+var $ = require('jquery');
+//var Person = require('./modules/Person'); ES5...
+import Person from './modules/Person'; // ES6...
+
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes $0 in taxes.");
+  }
+}
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Adult("Jane Smith", "orange");
+jane.greet();
+jane.payTaxes();
+
+$("h1").remove();
+
+*/
---
+++ app/assets/scripts/modules/Person.js
@@ -0,0 +1,17 @@
+// Example file
+/*
+class Person {
+  constructor(fullName, favColor) {
+    this.name = fullName;
+    this.favoriteColor = favColor;
+  }
+
+
+  greet() {
+    console.log("Hi there!, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+  }
+}
+
+// module.exports = Person; ES5
+export default Person;
+*/
